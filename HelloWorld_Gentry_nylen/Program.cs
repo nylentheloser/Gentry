3962527211b843c004da574fd07350ca4c6a6417
nylentheloser <nylengentry08@gmail.com>
2025-02-24 10:22:36 -0600

    Add project files.

---
+++ HelloWorld_Gentry_nylen/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld_Gentry_nylen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("(Optional)Please enter your middle name: ");
+            string middleName = Console.ReadLine();
+
+
+            Console.Write("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("What is your favorite food? ");
+            string favoriteFood = Console.ReadLine();
+
+
+            Console.WriteLine($"Hello {firstName} {middleName} {lastName}, lets go get {favoriteFood} later!");
+        }
+    }
+}
